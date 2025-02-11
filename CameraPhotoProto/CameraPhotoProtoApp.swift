170760274d879fedb347bd068bdd7fdf33d68aae
jorted-runner <jorted.python@gmail.com>
2025-02-11 15:44:59 -0600

    Initial Commit

---
+++ CameraPhotoProto/CameraPhotoProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPhotoProtoApp.swift
+//  CameraPhotoProto
+//
+//  Created by Danny Ellis on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraPhotoProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
